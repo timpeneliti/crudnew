1b404a32545aac36100fb08995defaf180d220c3
timpeneliti <136672930+timpeneliti@users.noreply.github.com>
2024-05-27 10:49:44 +0700

    menambah halaman login
    admin 123

--- app/src/main/java/com/example/crudnew/MainActivity.kt
+++ app/src/main/java/com/example/crudnew/MainActivity.kt
@@ -6,9 +6,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -28,13 +29,10 @@ class MainActivity : ComponentActivity() {
                 ) {
                     Greeting(name = "Android")
                     Spacer(modifier = Modifier.height(16.dp))
-                    Button(
-                        onClick = {
+                    Login { isLoginSuccessful ->
+                        if (isLoginSuccessful) {
                             startActivity(Intent(this@MainActivity, SecondActivity::class.java))
-                        },
-                        modifier = Modifier.fillMaxWidth()
-                    ) {
-                        Text("Go to Second Activity")
+                        }
                     }
                 }
             }
@@ -50,10 +48,41 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    Greeting("Android")
+fun Login(onLoginResult: (Boolean) -> Unit) {
+    var username by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        OutlinedTextField(
+            value = username,
+            onValueChange = { username = it },
+            label = { Text("Username") },
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        OutlinedTextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text("Password") },
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(
+            onClick = {
+                // Tambahkan logika validasi login di sini
+                val isLoginSuccessful = username == "admin" && password == "123"
+                onLoginResult(isLoginSuccessful)
+            },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("Login")
+        }
+    }
 }
 
 @Preview(showBackground = true)
